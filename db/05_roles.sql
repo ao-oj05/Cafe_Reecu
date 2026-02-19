13693e52784d23feb36d4859b256e225c9f10b8c
ao-oj05 <aliciaocana49@gmail.com>
2026-02-18 23:27:50 -0600

    Aquí econsultas sql

--- db/05_roles.sql
+++ db/05_roles.sql
@@ -1,8 +1,3 @@
--- =====================================================
--- db/roles.sql
--- Creación de rol app_user y permisos sobre VIEWS
--- =====================================================
-
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'app_user') THEN
@@ -18,14 +13,5 @@ GRANT SELECT ON
     vw_top_products_ranked,
     vw_inventory_risk,
     vw_customer_value,
-    vw_payment_mix,
-    vw_category_performance,
-    vw_orders_channel_daily
+    vw_payment_mix
     TO app_user;
-
-REVOKE ALL ON TABLE categories  FROM app_user;
-REVOKE ALL ON TABLE products    FROM app_user;
-REVOKE ALL ON TABLE customers   FROM app_user;
-REVOKE ALL ON TABLE orders      FROM app_user;
-REVOKE ALL ON TABLE order_items FROM app_user;
-REVOKE ALL ON TABLE payments    FROM app_user;
